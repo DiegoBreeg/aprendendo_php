3bab448d6ac1d04f6334af673ac5fe97698b969f
diego breeg <diegobreeg@gmail.com>
2023-12-06 15:00:53 -0300

    booleans

---
+++ 03_tipos/boolean.php
@@ -0,0 +1,12 @@
+<?php
+//O tipo bool tem apenas dois valores e é usado para expressas um valor de verdade.
+//pode ser true ou false.
+
+//Para especificar um bool literal, use as palavras-chave true ou false.
+//Ambas são case-insensitive.
+
+$foo = TRUE;
+
+//Tipicamente, o resultado de um operador que retorne um valor bool,
+//é passado para uma estrutura de controle.
+
---
+++ 03_tipos/null.php
@@ -0,0 +1,4 @@
+<?php
+//O tipo null é o tipo unitário do PHP, ou seja, possui apenas um valor: null
+$var = NULL;
+
