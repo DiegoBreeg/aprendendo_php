84354a07f1f07ef9581fef67e22462ecda64a3e7
diego breeg <diegobreeg@gmail.com>
2023-12-06 12:49:14 -0300

    primeiro commit

--- 03_tipos/intro.php
+++ 03_tipos/intro.php
@@ -1,13 +1,19 @@
 <?php
-$a_bool = true;
-$a_string = "foo";
-$a_string2 = 'foo';
-$an_inteiro = 12;
+$a_bool = true; // um valor boleano
+$a_string = "foo"; //um texto
+$a_string2 = 'foo'; //um texto
+$an_inteiro = 12; //um inteiro
 
 echo get_debug_type($a_bool), "\n";
 echo get_debug_type($a_string), "\n";
 
+//se essa variável conter um inteiro, aumente o número por quatro
 if(is_int($an_inteiro)) {
     $an_inteiro += 4;
 }
 var_dump($an_inteiro);
+
+//se $a_bool for um texto, imprima
+if(is_string($an_inteiro)) {
+    echo "String: $a_bool";
+}
