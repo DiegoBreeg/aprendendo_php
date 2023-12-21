8134b082a32b0d5355f54afb5b307fd70a1c295e
diego breeg <diegobreeg@gmail.com>
2023-12-21 12:25:28 -0300

    strings

--- 03_tipos/string.php
+++ 03_tipos/string.php
@@ -1,3 +1,22 @@
 <?php
 $s = "pensamentos não autorizados";
 echo "os sonhos são como $s";
+
+class Pessoa {
+    public $john = "João Silva";
+    public $jane = "Jane Smith";
+}
+
+$pessoa = new pessoa();
+
+echo $pessoa->john;
+
+class foo {
+    var $bar = 'I am bar.';
+}
+
+$foo = new foo();
+$bar = 'bar';
+$baz = array('foo', 'bar', 'baz', 'quux');
+echo "{$foo->bar}\n";
+echo "{$foo->{$baz[1]}}";
